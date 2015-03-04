cf6bcb657c1b4d7ff097db31c53626f02518f913
Clive Cox <cc@seldon.io>
2015-03-04 15:34:14 +0000

    change xargs usage

--- examples/create_ner_index.sh
+++ examples/create_ner_index.sh
@@ -17,7 +17,7 @@ java -cp ../target/semvec-lucene-tools-1.2-jar-with-dependencies.jar io.seldon.s
 java -cp ../target/semvec-lucene-tools-1.2-jar-with-dependencies.jar pitt.search.semanticvectors.BuildIndex -trainingcycles 1 -maxnonalphabetchars -1 -minfrequency 0 -maxfrequency 1000000 -luceneindexpath wiki_film_index
 
 #example query - "films related to Brad Pitt"
-java -cp ../target/semvec-lucene-tools-1.2-jar-with-dependencies.jar pitt.search.semanticvectors.Search -queryvectorfile termvectors.bin -searchvectorfile docvectors.bin brad_pitt | cut -d: -f2 | xargs -i echo "select name from items where item_id='{}';" | mysql --skip-column-names -u${MYSQL_USER} -p${MYSQL_PASS} -h${MYSQL_HOST} ${DB_NAME}
+java -cp ../target/semvec-lucene-tools-1.2-jar-with-dependencies.jar pitt.search.semanticvectors.Search -queryvectorfile termvectors.bin -searchvectorfile docvectors.bin brad_pitt | cut -d: -f2 | xargs -I{} echo "select name from items where item_id='{}';" | mysql --skip-column-names -u${MYSQL_USER} -p${MYSQL_PASS} -h${MYSQL_HOST} ${DB_NAME}
 
 
 
