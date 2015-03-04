aacb9232ed91bf8e3b843d69f67abbd1560378f3
Gurminder Sunner <gsunner2000@gmail.com>
2015-03-04 17:55:23 +0000

    csvtool replaced with a custom cvs column extractor

--- examples/create_wiki_film_db.sh
+++ examples/create_wiki_film_db.sh
@@ -3,6 +3,8 @@
 set -o nounset
 set -o errexit
 
+STARTUP_DIR="$( cd "$( dirname "$0" )" && pwd )"
+
 source ./common_vars.sh
 
 wget http://web.informatik.uni-mannheim.de/DBpediaAsTables/csv/Film.csv.gz
@@ -10,17 +12,10 @@ wget http://web.informatik.uni-mannheim.de/DBpediaAsTables/csv/Film.csv.gz
 gunzip Film.csv.gz
 
 echo "create database ${DB_NAME}" | mysql -u${MYSQL_USER} -p${MYSQL_PASS} -h${MYSQL_HOST}
-mysql -uroot ${DB_NAME} < ../schema-minimal.sql
+mysql -u${MYSQL_USER} -p${MYSQL_PASS} -h${MYSQL_HOST} ${DB_NAME} < ../schema-minimal.sql
 echo "insert into item_attr values (1,'film_abstract','VARCHAR',1);" | mysql -u${MYSQL_USER} -p${MYSQL_PASS} -h${MYSQL_HOST} ${DB_NAME}
 echo "add film ids and titles"
-cat Film.csv | csvtool -t ',' col 2 - | awk '{i=i+1;gsub("\"","", $0);;printf("insert into items values (%d,\"%s\",1);\n",i,$0)}' | mysql -u${MYSQL_USER} -p${MYSQL_PASS} -h${MYSQL_HOST} ${DB_NAME}
+cat Film.csv | ${STARTUP_DIR}/extract-csv-column -c 2 | awk '{i=i+1;gsub("\"","", $0);;printf("insert into items values (%d,\"%s\",1);\n",i,$0)}' | mysql -u${MYSQL_USER} -p${MYSQL_PASS} -h${MYSQL_HOST} ${DB_NAME}
 echo "add film abstracts"
-cat Film.csv | csvtool -t ',' col 3 - | awk '{i=i+1;gsub("\"","", $0);;printf("insert into item_map_text values (%d,1,\"%s\");\n",i,$0)}' | mysql -u${MYSQL_USER} -p${MYSQL_PASS} -h${MYSQL_HOST} ${DB_NAME}
-
-
-
-
-
-
-
+cat Film.csv | ${STARTUP_DIR}/extract-csv-column -c 3 | awk '{i=i+1;gsub("\"","", $0);;printf("insert into item_map_text values (%d,1,\"%s\");\n",i,$0)}' | mysql -u${MYSQL_USER} -p${MYSQL_PASS} -h${MYSQL_HOST} ${DB_NAME}
 
